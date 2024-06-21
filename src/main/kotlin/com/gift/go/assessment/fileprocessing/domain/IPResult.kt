1d528146333ba2f3f1b07e429c22783bca0bdc19
skkcoder <165959722+skkcoder@users.noreply.github.com>
2024-06-21 11:03:16 +0100

    adding country code

--- src/main/kotlin/com/gift/go/assessment/fileprocessing/domain/IPResult.kt
+++ src/main/kotlin/com/gift/go/assessment/fileprocessing/domain/IPResult.kt
@@ -2,7 +2,14 @@ package com.gift.go.assessment.fileprocessing.domain
 
 sealed class IPResult
 
-data class IPInformation(val isp: String, val org: String, val `as`: String, val country: String, val status: String) :
+data class IPInformation(
+    val isp: String,
+    val org: String,
+    val `as`: String,
+    val country: String,
+    val countryCode: String,
+    val status: String
+) :
     IPResult()
 
 data class IPFail(val query: String, val message: String, val status: String) : IPResult()
