c2ec29dba03fa708f534ceffb5867108e93da64e
skkcoder <165959722+skkcoder@users.noreply.github.com>
2024-06-23 23:54:29 +0100

    flyway and persisting audit information

--- src/main/kotlin/com/gift/go/assessment/security/repositories/SecurityAuditRepository.kt
+++ src/main/kotlin/com/gift/go/assessment/security/repositories/SecurityAuditRepository.kt
@@ -4,4 +4,4 @@ import com.gift.go.assessment.security.domain.SecurityAuditInformation
 import java.util.*
 import org.springframework.data.repository.CrudRepository
 
-interface SecurityAuditRepository : CrudRepository<SecurityAuditInformation, UUID> {}
+interface SecurityAuditRepository : CrudRepository<SecurityAuditInformation, UUID>
